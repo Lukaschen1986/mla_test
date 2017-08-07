63f65a107ed44c96cb6f5e02e89d7093c10b771f
Chen Wendong <chen_wen_dong@126.com>
2017-08-07 10:30:22 +0800

    Update bpnn_cls.py

--- bpnn_cls.py
+++ bpnn_cls.py
@@ -11,7 +11,9 @@ from sklearn import linear_model
 from sklearn.neural_network import MLPClassifier
 import matplotlib.pyplot as plt
 
-class BPNetworkCls(object):
+unit_vec = lambda x: x/np.sqrt(np.sum(x**2))
+
+class BpnnTrain(object):
     def __init__(self, X, y, layer_num, neuron_num, outNum, yita, lam, iter_num):
         self.X = X
         self.y = y
@@ -23,8 +25,6 @@ class BPNetworkCls(object):
         self.iter_num = iter_num
         self.n = len(self.X) # 样本个数
         self.p = self.X.shape[1] # 样本维度
-        
-    unit_vec = lambda x: x/np.sqrt(np.sum(x**2))
     
     def layer_func(self):
         w_res = []; b_res = [] # 初始化w, b
@@ -68,8 +68,60 @@ class BPNetworkCls(object):
             lossFunc += self.lam/2*np.sum(w[i]**2)
         return lossFunc
     
-obj = BPNetworkCls(X=X_train, y=y_train, layer_num=4, neuron_num=4, outNum=10, yita=0.001, lam=0.001, iter_num=1000)
-w, b = obj.layer_func()
-prob, a_res = obj.forward_func(w,b)
+    def backward_func(self, prob, a, w):
+        dw_res = []; db_res = []
+        delta = prob
+        delta[range(self.n), self.y] -= 1 # 对输出层求导
+        for i in range(len(w))[::-1]: # 逐层求导，注意是倒序
+            dw = a[i].T.dot(delta) + self.lam*w[i]
+            db = np.sum(delta, axis=0, keepdims=True)
+            dw_res.append(dw)
+            db_res.append(db)
+            delta = delta.dot(w[i].T) * (1-a[i]**2)
+        # 把倒序转成正序 
+        dw_res = dw_res[::-1]
+        db_res = db_res[::-1]
+        return dw_res, db_res
 
+    def grad_descent(self, dw, db, w, b):
+        for i in range(len(w)):
+            w[i] -= self.yita*unit_vec(dw[i])
+            b[i] -= self.yita*unit_vec(db[i])
+        return w, b
+
+    def iter_func(self):
+        w, b = self.layer_func()
+        lossFunc_res = []
+        for i in range(self.iter_num):
+            prob, a = self.forward_func(w, b)
+            lossFunc = self.loss_func(prob, w)
+            lossFunc_res.append(lossFunc)
+            if lossFunc_res[i] > lossFunc_res[i-1]: break
+            # 反向逐层求导
+            dw, db = self.backward_func(prob, a, w)
+            # 梯度下降更新w,b
+            w, b = self.grad_descent(dw, db, w, b)
+        return w, b, lossFunc_res
+    
+obj_train = BpnnTrain(X=X_train, y=y_train, layer_num=4, neuron_num=60, outNum=10, yita=0.001, lam=0.001, iter_num=10000)
+w, b, lossFunc_res = obj_train.iter_func()
+plt.plot(lossFunc_res)
+min(lossFunc_res)
+np.argmin(lossFunc_res)
+
+class BpnnTest(BpnnTrain):
+    def __init__(self, w, b, X, y):
+        self.w = w
+        self.b = b
+        self.X = X
+        self.y = y
+    
+    def test_func(self):
+        prob, a = BpnnTrain.forward_func(self, w, b)
+        y_hat = np.argmax(prob, axis=1)
+        accu = np.sum(y_hat == self.y)/len(self.y)
+        return y_hat, accu
+
+obj_test = BpnnTest(w, b, X_test, y_test)
+y_hat, accu = obj_test.test_func()
 
