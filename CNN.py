3436ed16df6d07aa9a3d2718d595b210a214976b
Lukas_Chen <chen_wen_dong@126.com>
2017-08-17 19:31:25 +0800

    Create CNN.py

---
+++ CNN.py
@@ -0,0 +1,5 @@
+卷积神经网络的数学推导: https://wenku.baidu.com/view/deb7be2b6137ee06eef918a0.html
+卷积神经网络简介: https://zhuanlan.zhihu.com/p/25249694
+神经网络之梯度下降与反向传播（上）: https://zhuanlan.zhihu.com/p/25387613?group_id=819934784259256320
+神经网络之梯度下降与反向传播（下）: https://zhuanlan.zhihu.com/p/25609953?refer=pillgrim
+优化迭代算法简介: http://blog.csdn.net/silent56_th/article/details/59680033
