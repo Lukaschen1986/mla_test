ad3c14b62dea215eb28ce228c177171090b230d2
Lukas_Chen <chen_wen_dong@126.com>
2017-08-30 18:02:50 +0800

    Update svm.py

--- svm.py
+++ svm.py
@@ -19,11 +19,53 @@ n, p = X.shape[0], X.shape[1]
 a = np.zeros((n,1))
 b = 0
 C = 0.6
-max_iter = 40
+max_iters = 40
 
 a_change = 0
 for i in range(n):
     # i = 0
     y_hat = a[i]*y[i]*X[i].dot(X[i]) + b
     Ei = y_hat - y[i]
-    if y[i]*y_hat > 1 and a > 0 or y[i]*y_hat == 1 and a == 0 or a == C
+    # 违反KKT
+    if y[i]*y_hat > 1 and a[i] > 0 or y[i]*y_hat < 1 and a[i] < C:
+        
+        
+
+def random_idx(n, j):
+    i = j
+    while i == j:
+        i = np.random.randint(0,n)
+    return i
+        
+K = (1 + 0.001*X.dot(X.T))**2
+
+def find_bounds(y, i, j):
+    if y[i] == y[j]:
+        L = max(0, a[i] + a[j] - C)
+        H = min(a[i] + a[j], C)
+    else:
+        L = max(0, a[j] - a[i])
+        H = min(C, C - a[i] + a[j])
+    return L, H
+        
+np.arange(0,n)
+
+def train():
+    iters = 0
+    while iters < max_iters:
+        iters += 1
+        a_prev = np.copy(a)
+    
+    for j in range(n): # j = 0
+        # Pick random i
+        i = random_idx(n, j)
+        
+        eta = K[i,i] + K[j,j] - 2*K[i,j]
+        if eta <= 0:
+            continue
+        L, H = find_bounds(y, i, j)
+        
+        # Error for current examples
+        y_hat = a[i]*y[i]*X[i].dot(X.T) + b
+
+np.random.randint(0,n-1)
