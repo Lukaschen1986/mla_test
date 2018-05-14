cfab1c77c45fd2cbe02d46ac2d31a478fa193179
Lukas_Chen <chen_wen_dong@126.com>
2018-05-14 19:14:31 +0800

    Update conn.py

--- conn.py
+++ conn.py
@@ -31,6 +31,7 @@ conn.commit()
 df = pd.read_sql(sql="", con=conn)
 conn.close()
 
+# 入库（删表重建）
 engine = create_engine("mysql+pymysql://slusr:WW6LYvC6@10.171.199.172:3306/sl01?charset=utf8")
 df_iair_historyprice.to_sql(name="t_iair_historyprice", con=engine, if_exists="replace", chunksize=None, 
                             index=True, index_label="RecordID",
@@ -44,3 +45,30 @@ df_iair_historyprice.to_sql(name="t_iair_historyprice", con=engine, if_exists="r
                                    "CurrencyCode": types.VARCHAR(3),
                                    "CreateTime": types.DATETIME(),
                                    "LastModifyTime": types.TIMESTAMP()})
+
+# 入库（不删表）
+def insert_data(self, df_res, sql_truncate, sql_insert):
+    isNaN = lambda x: x!=x
+    # conn
+    itravelhw = pymysql.connect(self.host, self.user, self.passwd, self.db, self.port, charset="utf8")
+    itravelhw.commit()
+    cur = itravelhw.cursor()
+    # truncate exsit data
+    cur.execute(sql_truncate)
+    itravelhw.commit()
+    # insert new data
+    array_res = df_res.values
+    t0 = pd.Timestamp.now()
+    for i in range(len(array_res)):
+        val = array_res[i].tolist()
+
+        if isNaN(val[6]):
+            val[6] = None
+
+        cur.execute(sql_insert, val)
+        itravelhw.commit()
+
+    t1 = pd.Timestamp.now()
+    cur.close()
+    itravelhw.close()
+    print("数据入库完毕，用时：%s" % str(t1-t0))
