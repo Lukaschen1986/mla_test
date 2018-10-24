d5d64c13ea2ddfe046d014e4d2ba214d73bf8851
Lukas_Chen <chen_wen_dong@126.com>
2018-10-24 11:56:54 +0800

    Update AdaBoostRegressor.py

--- statistical_learning/AdaBoostRegressor.py
+++ statistical_learning/AdaBoostRegressor.py
@@ -24,31 +24,64 @@ x_test = np.array(df_test.iloc[:,1:])
 y_test = np.array(df_test.iloc[:,0])
 
 
-weights = init_weights(x_train)
-x_train_update = np.expand_dims(weights, axis=1) * x_train
+def init_weights(x):
+    nrow = len(x)
+    init_value = 1.0 / nrow
+    weights = np.tile(init_value, nrow)
+    return weights
 
-
-dtr = DecisionTreeRegressor(criterion="mse",
-                            max_depth=4,
-                            min_samples_split=2,
-                            min_samples_leaf=1,
-                            max_features="sqrt",
-                            random_state=0)
-reg_dtr_model = dtr.fit(x_train_update, y_train)
-y_hat_train = reg_dtr_model.predict(x_train_update)
-
-eps_max = np.max(np.abs(y_train-y_hat_train))
-
-#eps_i = 1 - np.exp((-y_train+y_hat_train) / eps_max)
-#eps_i = (y_train - y_hat_train)**2 / eps_max
-eps_i = np.abs(y_train - y_hat_train) / eps_max
-
-
-eps = eps_i.dot(weights)
-
-alpha = eps / (1-eps)
-
-z = alpha**(1-eps_i).dot(weights)
-
-weights = weights * alpha**(1-eps_i) / z
-
+def update_epsilon(weights, y_train, y_hat):
+    eps_max = np.max(np.abs(y_train-y_hat))
+    eps_i = np.abs(y_train - y_hat) / eps_max
+    eps = np.sum(eps_i * weights)
+    return eps, eps_i
+  
+def update_alpha(eps):
+    alpha = eps/(1.0-eps)
+    return alpha
+  
+def update_weights(weights, y_train, y_hat, alpha, eps_i):
+    z = np.sum(weights * alpha**(1-eps_i))
+    weights = weights * alpha**(1-eps_i) / z
+    return weights
+  
+def fit(x_train_update, y_train, tree_depth, max_iter):
+    # init_weights
+    weights = init_weights(x_train)
+    x_train_update = np.expand_dims(weights, axis=1) * x_train
+    # bulid model
+    reg_dt = DecisionTreeRegressor(criterion="mse",
+                                   max_depth=tree_depth,
+                                   min_samples_split=2,
+                                   min_samples_leaf=1,
+                                   max_features=None,
+                                   random_state=0)
+    reg_dt_model_res = []; eps_res = []; alpha_res = []
+    
+    for i in range(max_iter):
+        # fit model
+        reg_dt_model = reg_dt.fit(x_train_update, y_train)
+        # save model
+        reg_dt_model_res.append(reg_dt_model)
+        # predict on train
+        y_hat_train = reg_dt_model.predict(x_train_update)
+        # update_epsilon
+        eps, eps_i = update_epsilon(weights, y_train, y_hat_train)
+        eps_res.append(eps)
+        # update_alpha
+        alpha = update_alpha(eps)
+        alpha_res.append(alpha)
+        # update_weights
+        weights = update_weights(weights, y_train, y_hat_train, alpha, eps_i)
+        # x_train_update
+        x_train_update = np.expand_dims(weights, axis=1) * x_train
+    return reg_dt_model_res, eps_res, alpha_res
+  
+  
+def predict(x_test, reg_dt_model_res, alpha_res):
+    y_pred = 0
+    
+    for i in range(max_iter):
+        reg_dt_model = reg_dt_model_res[i]
+        y_hat = reg_dt_model.predict(x_test_update)
+        y_pred += np.log(1.0/alpha_res[i]) * y_hat
