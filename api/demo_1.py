3e36c7fe3a6808a38287a5424eb6fc81c74f6bf4
Lukas_Chen <chen_wen_dong@126.com>
2018-12-12 21:44:47 +0800

    Create demo_1.py

---
+++ api/demo_1.py
@@ -0,0 +1,28 @@
+# -*- coding: utf-8 -*-
+from flask import Flask
+from flask_script import Manager # 命令行参数接收 shell, runserver
+
+app = Flask(__name__)
+manager = Manager(app=app)
+
+@app.route("/demo")
+def demo_func():
+    return "Hello World!"
+
+@app.route("/params/<idx>/")
+def params_func(idx):
+    print(idx)
+    return "success"
+
+@app.route("/get/<string:name>/")
+def get_func(name):
+    print(name)
+    return "get success"
+
+
+
+
+
+if __name__ == "__main__":
+#    app.run(host="127.0.0.1", port=5000, debug=True)
+    manager.run() # python demo_1.py runserver -d -r -h 127.0.0.1 -p 5000
