b1e366f1173cb2862eed46e143af43ff78b446f4
Lukas_Chen <chen_wen_dong@126.com>
2019-02-10 19:30:04 +0800

    Create config.py

---
+++ api/tornado/config.py
@@ -0,0 +1,7 @@
+# -*- coding: utf-8 -*-
+CONN_DICT = {"host": "127.0.0.1",
+             "user": "root",
+             "passwd": "chen1986",
+             "port": 3306,
+             "db": "test",
+             "charset": "utf8"}
