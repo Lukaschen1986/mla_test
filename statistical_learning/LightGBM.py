c110f4587982792683464439be5306744b4d91af
Lukas_Chen <chen_wen_dong@126.com>
2019-04-15 16:05:56 +0800

    Update LightGBM.py

--- statistical_learning/LightGBM.py
+++ statistical_learning/LightGBM.py
@@ -20,11 +20,11 @@ LightGBM can use categorical features as input directly. It doesn’t need to co
 Note: You should convert your categorical features to int type before you construct Dataset.
 '''
 
-def get_lgb_data(x_train_sparse, y_train, x_test_sparse, y_test):
-    d_train = lgb.Dataset(data=x_train_sparse, label=y_train)
-    d_test = lgb.Dataset(data=x_test_sparse, label=y_test)
-    #watchlist = [(d_train, "train"), (d_test, "test")]
-    return d_train, d_test
+ddef get_lgb_data(x_train_sparse, y_train, x_valid_sparse, y_valid, feat_names):
+    d_train = lgb.Dataset(data=x_train_sparse, label=y_train, feature_name=feat_names)
+    d_valid = lgb.Dataset(data=x_valid_sparse, label=y_valid, feature_name=feat_names, reference=d_train)
+    valid_sets = [d_valid, d_train]
+    return d_train, valid_sets
 
 
 def get_best_estimators(d_train, params_lgb_1, params_lgb_2, k, verbose=True):
@@ -33,11 +33,12 @@ def get_best_estimators(d_train, params_lgb_1, params_lgb_2, k, verbose=True):
                       train_set=d_train, 
                       num_boost_round=params_lgb_2["n_estimators"],
                       nfold=k, #k=3
+                      stratified=False,
                       metrics="rmse", 
                       early_stopping_rounds=int(params_lgb_2["n_estimators"]/10),
                       verbose_eval=verbose)
     # update params
-    n_estimators = model_cv.shape[0]
+    n_estimators = len(model_cv["rmse-mean"])
     print("update n_estimators to: {}".format(n_estimators))
     params_lgb_1.set_params(n_estimators=n_estimators)
     params_lgb_2 = params_lgb_1.get_params()
@@ -58,28 +59,31 @@ def get_best_params(x_train_sparse, y_train, params_lgb_1, params_lgb_2, k, para
     return params_lgb_1, params_lgb_2
 
 
-def train_model(d_train, d_test, params_lgb_2, verbose):
+def train_model(d_train, valid_sets, params_lgb_2, verbose):
     model_lgb = lgb.train(params=params_lgb_2,
                           train_set=d_train,
-                          valid_set=d_test,
+                          valid_sets=valid_sets,
+                          valid_names=["valid","train"],
                           num_boost_round=params_lgb_2["n_estimators"],
                           early_stopping_rounds=int(params_lgb_2["n_estimators"]/10),
                           verbose_eval=verbose)
     return model_lgb
 
 
-def train(x_train_sparse, y_train, x_test_sparse, y_test, n_estimators_0, objective, eval_metric, k, verbose):
+@caltime
+def train_lgb(x_train_sparse, y_train, x_valid_sparse, y_valid, feat_names,
+              n_estimators_0, objective, eval_metric, k, verbose):
     # get_lgb_data
-    d_train, d_test = get_lgb_data(x_train_sparse, y_train, x_test_sparse, y_test)
+    d_train, valid_sets = get_lgb_data(x_train_sparse, y_train, x_valid_sparse, y_valid, feat_names)
     # define init params
-    params_lgb_1 = LGBMRegressor(booster="gbdt",
-                                 silent=True,
+    params_lgb_1 = LGBMRegressor(boosting_type="gbdt",
+                                 #silent=True,
                                  n_jobs=-1,
                                  learning_rate=0.1,
                                  n_estimators=n_estimators_0, #n_estimators_0=100
                                  min_gain_to_split=0, #gamma=0,
-                                 max_depth=9,
-                                 #num_leaves=31,
+                                 #max_depth=9,
+                                 num_leaves=31,
                                  min_child_weight=0.001,
                                  bagging_fraction=0.9, #subsample=0.9,
                                  feature_fraction=0.9, #colsample_bytree=0.9,
@@ -97,19 +101,22 @@ def train(x_train_sparse, y_train, x_test_sparse, y_test, n_estimators_0, object
     params_lgb_1, params_lgb_2 = get_best_params(x_train_sparse, y_train, params_lgb_1, params_lgb_2, k,
                                                  params_hyp=dict(learning_rate=[0.1, 0.2, 0.3]))
     print("learning_rate: ", params_lgb_2["learning_rate"])
-    # max_depth & min_child_weight
+    # num_leaves
     params_lgb_1, params_lgb_2 = get_best_params(x_train_sparse, y_train, params_lgb_1, params_lgb_2, k,
-                                                 params_hyp=dict(max_depth=[5,7,9,11], 
-                                                                 min_child_weight=[0.001, 0.01, 0.1, 1, 10]))
-    print("max_depth: {0}; min_child_weight: {1}".format(params_lgb_2["max_depth"], params_lgb_2["min_child_weight"]))
+                                                 params_hyp=dict(num_leaves=[15,31,65,127]))
+    print("num_leaves: {}".format(params_lgb_2["num_leaves"]))
+    # min_child_weight
+    params_lgb_1, params_lgb_2 = get_best_params(x_train_sparse, y_train, params_lgb_1, params_lgb_2, k,
+                                                 params_hyp=dict(min_child_weight=[0.001, 0.01, 0.1, 1, 10]))
+    print("min_child_weight: {}".format(params_lgb_2["min_child_weight"]))
     # min_gain_to_split / gamma
     params_lgb_1, params_lgb_2 = get_best_params(x_train_sparse, y_train, params_lgb_1, params_lgb_2, k,
                                                  params_hyp=dict(min_gain_to_split=[0, 0.5, 1, 1.5, 2, 2.5]))
     print("min_gain_to_split: ", params_lgb_2["min_gain_to_split"])
     # bagging_fraction / subsample & feature_fraction / colsample_bytree
     params_lgb_1, params_lgb_2 = get_best_params(x_train_sparse, y_train, params_lgb_1, params_lgb_2, k,
-                                                 params_hyp=dict(bagging_fraction=[0.6,0.7,0.8,0.9], 
-                                                                 feature_fraction=[0.6,0.7,0.8,0.9]))
+                                                 params_hyp=dict(bagging_fraction=np.arange(0.6,1.0,0.05), 
+                                                                 feature_fraction=np.arange(0.6,1.0,0.05)))
     print("bagging_fraction: {0}; feature_fraction: {1}".format(params_lgb_2["bagging_fraction"],params_lgb_2["feature_fraction"]))
     # lambda_l1 / reg_alpha
     params_lgb_1, params_lgb_2 = get_best_params(x_train_sparse, y_train, params_lgb_1, params_lgb_2, k,
@@ -127,12 +134,15 @@ def train(x_train_sparse, y_train, x_test_sparse, y_test, n_estimators_0, object
     # get_best_estimators
     params_lgb_1, params_lgb_2 = get_best_estimators(d_train, params_lgb_1, params_lgb_2, k, verbose)
     # train_model
-    model_lgb = train_model(d_train, d_test, params_lgb_2, verbose)
+    model_lgb = train_model(d_train, valid_sets, params_lgb_2, verbose)
     print("LGB model train complete.")
     # save model
+    model_lgb.save_model(filename="model_lgb.model")
+    print("model has saved.")
+    # load model
+    #model_lgb = lgb.Booster(model_file="model_lgb.model")
     # with open(workpath + "/data/models/{0}/model_lgb_{1}.txt".format(self.datasource,self.end_time), "wb") as f:
     # pickle.dump(model_lgb, f)
-    # print("model has saved.")
     # with open(workpath + "/data/hyper/{}/params_lgb_1.txt".format(self.datasource), "wb") as f:
     # pickle.dump(params_lgb_1, f)
     # print("param has saved.")
